547b883107f314e9b7f4100bfd22b17388d2252b
kishorssk <kishor.sathigrama@gmail.com>
2019-08-22 18:49:18 +0530

    Removed arrows

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -11,13 +11,13 @@
 <img class="img-fluid" src="html/images/slide2.jpg" alt="Second slide">
 </div> -->
 </div>
-<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+<!-- <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
   <span class="sr-only">Previous</span>
 </a>
 <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
   <span class="carousel-control-next-icon" aria-hidden="true"></span>
-  <span class="sr-only">Next</span>
+  <span class="sr-only">Next</span> -->
 </a>
 </div>
 <div class="container first-stage">
