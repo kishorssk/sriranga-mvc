0fbdd2b9d2e985e3683bf7c7e0f15a1f30801235
Shivashankar <shivably04sdst@gmail.com>
2019-10-28 18:28:26 +0530

    News added

---
+++ application/views/flat/news/index.php
@@ -0,0 +1,17 @@
+<div class="container second-stage">
+	<div class="row">
+		<div class="col-md-3">
+			<img src="<?=BASE_URL?>public/images/mainc/001.jpg" class="img-fluid" alt="Sri Shankara Granthavali cover image" />
+		</div>	
+		<div class="col-md-9">
+			<h3 class="text-center">Sri Shankara Granthavali</h3> 
+			<h4 class="text-center"><i>by</i> T.K. Balasubrahmanyam</h4>
+			<h4 class="text-center">E-books</h4>
+			<p class="text-center"> An authoritative Collected Works of Adi Sankara!</p>
+			<p class="text-center mt-2"><button class="btn btn-success">Pre-Order</button></p>
+			<h4 class="mt-5">Commemorating</h4>
+			<h4>Gurubhaktashikhamani (गुरुभक्तशिखामणिः) Sri T K Balasubrahmanyam</h4>
+			<p>Born in 1874, Sri T K Balasubrahmanyam hailed from a prosperous family of Thanjavur. He showed a remarkable aptitude for scholarship when still young. Mastering Tamil, Sanskrit and English speedily, he graduated from S.P.G. College, Tiruchi. In 1904, he founded Sri Vani Vilas Press of Srirangam, which went onto become a well-known firm of publishers and printers of ancient and national literature. During His 1907-1910 Vijayayatra, the 33rd Jagadguru of Dakshinamnaya Sri Sharadapeetham, Sringeri, Sri Sacchidananda Shivabhinava Nrisimha Bharati Mahaswamiji graced the residence of Sri Balasubrahmanyam in Srirangam, for one day, sometime in 1908. Knowing about his Sri Vani Vilasa Press, the Jagadguru commanded Sri Balasubrahmanyam to bring out a good edition of Sadashiva Brahmendra's Brahmasutra vritti; this task was accomplished and the book brought out on 20 January 1909. During the chaturmasya that year in Papanasham, July-August 1909, Sri Balasubrahmanyam had the Darshan of the Jagaduguru, who commanded him to bring out a uniform and accurate edition of all the genuine works of Bhagavatpada Sri Shankaracharya. On 21st February 1910, Sri Balasubrahmanyam was able to place six volumes of Sri Shankara Granthavali during the Kumbhabhishekam of the Temple at Kalady, the birthplace of the Great World Teacher. Looking at the time taken to carry out the command of the Jagadguru and the quality of the volumes published, it can only be due to the Grace of the Jagadguru. This digital edition of the Memorial Edition of Sri Sankaracharya’s Works (20 Vols.) is brought out to commemorate the great accomplishment of Sri Balasubrahmanyam. Devangari font used here is from Adisila, which has been designed to resemble the typeface used in Vani Vilasa publications. It is hoped that this will pave the way to bring to life again all the publications of the Sri Vani Vilasa Press, Srirangam.</p>
+		</div>	
+	</div> 
+</div>
