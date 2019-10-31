80a58eeeea7a65a82f009d0a2550fb010618478e
Shivashankar <shivably04sdst@gmail.com>
2019-10-31 11:45:37 +0530

    news added to carousel

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -7,17 +7,17 @@
     <div class="carousel-item active">
       <img class="img-fluid" src="<?=IMAGE_URL?>slide1.jpg" alt="First slide">
     </div>
-<!-- <div class="carousel-item">
-<img class="img-fluid" src="html/images/slide2.jpg" alt="Second slide">
-</div> -->
+	<div class="carousel-item">
+		<img class="img-fluid" src="<?=IMAGE_URL?>slide2.jpg" alt="Second slide">
+	</div>
 </div>
-<!-- <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
   <span class="sr-only">Previous</span>
 </a>
 <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
   <span class="carousel-control-next-icon" aria-hidden="true"></span>
-  <span class="sr-only">Next</span> -->
+  <span class="sr-only">Next</span>
 </a>
 </div>
 <div class="container first-stage">
