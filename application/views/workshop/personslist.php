b2cad8983b39ee2afe7c68bb2894c182afc1c05b
Shivashankar <shivably04sdst@gmail.com>
2019-11-30 14:28:39 +0530

    bug fix in listing persons

--- application/views/workshop/personslist.php
+++ application/views/workshop/personslist.php
@@ -8,14 +8,14 @@
 			<h2>&nbsp;</h2>
 		</div>
 		<div clss="col-md-12">
-			<?php foreach($data as $row) ?>
+			<?php foreach($data as $row) {?>
 				<p>
 					Name: <b><?=$row['name']?></b><br />
 					College: <b><?=$row['college']?></b><br />
 					Mobile No: <b><?=$row['phonenumber']?></b>
 					<hr>
 				</p>				
-			<?php ?>
+			<?php } ?>
 		</div>
 	</div>
 </div>			
