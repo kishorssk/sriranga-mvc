1222d3d35c6d4aec5e131d68cdd26de92b97e6b8
Shivashankar <shivably04sdst@gmail.com>
2019-12-07 18:33:39 +0530

    Added highlights page

---
+++ application/views/flat/highlights/index.php
@@ -0,0 +1,125 @@
+<div class="container second-stage">
+	<div class="row">
+		<div class="col-md-12">
+			<h3 class="text-center">Shankara Granthavali &mdash; Product Overview</h3>
+			<ol>
+				<li>
+					<h6>About Page</h6>
+					<ul>
+						<li><i>Concluding Note</i>  &mdash; T.K. Balasubrahmanyam</li>
+						<li><i>An Appreciation</i>  &mdash; N. Srikantha Sastri</li>
+						<li><i>Superhuman efforts of a savant</i> &mdash; Prema Nandakumar</li>
+						<li><i>Views on the Sri Vani Vilas Press,</i> Srirangam</li>
+						<li><i>His (Sri TKB’s) aim</i></li>
+					</ul>
+				</li>
+				<li>
+					<h6>Volumes Page</h6>
+					<ul>
+						<li>Browse by volumes</li>
+					</ul>
+				</li>
+				<li>
+					<h6>Works Page</h6>
+					<ul><li>Browse by name of the work (For example, Aparokshanubhuti)</li></ul>
+				</li>
+				<li>
+					<h6>Search Page</h6>
+					<ul>
+						<li>Search the extensive table of contents of the complete works</li>
+						<li>Full text search</li>
+					</ul>
+				</li>
+				<li>
+					<h6>Cross Platform Application</h6>
+					<ul><li>Works on Linux, Winows and Mac operating systems</li></ul>
+				</li>
+			</ol>
+		</div>
+		<div class="col-md-12 mt-4">
+			<h5>Gallery</h5>
+			<p>One thing that stands out whenever one sees a book from the Vani Vilas Press is the love with which it is crafted – the lovely colourful border drawings, the font and the aesthetic typography, .... Here are a few samples from Shankara Granthavali.
+			</p>
+			<div id="images">
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0001.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0001.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0002.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0002.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0003.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0003.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0004.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0004.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0005.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0005.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0006.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0006.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0007.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0007.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0008.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0008.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0009.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0009.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0010.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0010.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0011.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0011.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0012.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0012.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0013.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0013.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0014.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0014.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0015.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0015.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0016.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0016.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0017.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0017.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+				<img data-original="<?=BASE_URL?>public/images/sggallery/0018.jpg" src="<?=BASE_URL?>public/images/sggallery/thumbs/0018.jpg"  class="mb-1 img-fluid img-thumbnail" alt="Sri Shankara Granthavali Gallery" />&nbsp;
+			</div>
+		</div>	
+		<div class="col-md-12 mt-4">
+			<h5>Screenshots</h5>
+			<div id="screenshots">
+				<img data-original="<?=BASE_URL?>public/images/sgscreenshots/Screenshot1.png" src="<?=BASE_URL?>public/images/sgscreenshots/thumbs/Screenshot1.png" class="mb-1 img-fluid img-thumbnail" alt="Shankara Granthavali - screenshots" />
+				<img data-original="<?=BASE_URL?>public/images/sgscreenshots/Screenshot2.png" src="<?=BASE_URL?>public/images/sgscreenshots/thumbs/Screenshot2.png" class="mb-1 img-fluid img-thumbnail" alt="Shankara Granthavali - screenshots" />
+				<img data-original="<?=BASE_URL?>public/images/sgscreenshots/Screenshot3.png" src="<?=BASE_URL?>public/images/sgscreenshots/thumbs/Screenshot3.png" class="mb-1 img-fluid img-thumbnail" alt="Shankara Granthavali - screenshots" />
+				<img data-original="<?=BASE_URL?>public/images/sgscreenshots/Screenshot4.png" src="<?=BASE_URL?>public/images/sgscreenshots/thumbs/Screenshot4.png" class="mb-1 img-fluid img-thumbnail" alt="Shankara Granthavali - screenshots" />
+				<img data-original="<?=BASE_URL?>public/images/sgscreenshots/Screenshot5.png" src="<?=BASE_URL?>public/images/sgscreenshots/thumbs/Screenshot5.png" class="mb-1 img-fluid img-thumbnail" alt="Shankara Granthavali - screenshots" />
+				<img data-original="<?=BASE_URL?>public/images/sgscreenshots/Screenshot6.png" src="<?=BASE_URL?>public/images/sgscreenshots/thumbs/Screenshot6.png" class="mb-1 img-fluid img-thumbnail" alt="Shankara Granthavali - screenshots" />
+				<img data-original="<?=BASE_URL?>public/images/sgscreenshots/Screenshot7.png" src="<?=BASE_URL?>public/images/sgscreenshots/thumbs/Screenshot7.png" class="mb-1 img-fluid img-thumbnail" alt="Shankara Granthavali - screenshots" />
+				<img data-original="<?=BASE_URL?>public/images/sgscreenshots/Screenshot8.png" src="<?=BASE_URL?>public/images/sgscreenshots/thumbs/Screenshot8.png" class="mb-1 img-fluid img-thumbnail" alt="Shankara Granthavali - screenshots" />
+				<img data-original="<?=BASE_URL?>public/images/sgscreenshots/Screenshot9.png" src="<?=BASE_URL?>public/images/sgscreenshots/thumbs/Screenshot9.png" class="mb-1 img-fluid img-thumbnail" alt="Shankara Granthavali - screenshots" />
+			</div>	
+		</div>
+	</div>
+</div>
+
+<!--
+<p><img style="width: 50%;" src="<?=BASE_URL?>public/images/sgscreeshots/sgscreenshot1.png" alt="Screenshot showing About page - Shankara Granthavali" class="img-fluid img-thumbnail" /></p>
+-->
+<script>
+var images = new Array ('coverpage1.jpg', 'coverpage2.jpg','coverpage3.jpg', 'coverpage4.jpg');
+var index = 1;
+ 
+function rotateImage()
+{
+  $('#myImage').fadeOut('fast', function()
+  {
+    $(this).attr('src', base_url+"public/images/mainc/" + images[index]);
+ 
+    $(this).fadeIn('fast', function()
+    {
+      if (index == images.length-1)
+      {
+        index = 0;
+      }
+      else
+      {
+        index++;
+      }
+    });
+  });
+}
+ 
+$(document).ready(function()
+{
+
+    if(document.getElementById('images')){
+        
+        var viewer = new Viewer(document.getElementById('images'), {url: 'data-original'});
+    }
+    if(document.getElementById('screenshots')){
+        
+        var viewer1 = new Viewer(document.getElementById('screenshots'), {url: 'data-original'});
+    }
+
+  //setInterval (rotateImage, 2500);
+
+});
+
+
+</script>
