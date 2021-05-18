48283b3a148cdaa991d2b6ed196eec22e27b9a01
Shivashankar <shivably04sdst@gmail.com>
2021-05-18 15:00:33 +0530

    SG page updated with account details

--- application/views/flat/SG/index.php
+++ application/views/flat/SG/index.php
@@ -58,6 +58,17 @@
 				20 Volumes (1910-14)<br />
 			</p>
 			<p class="text-center mt-2"><button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-success">Order online</button><br /></p>
+			<p class="my-4"><strong>If you have difficulty in completing the pay online  process, Please send  the  amount to the following account and write an email to sriranga.yoga@gmail.com with payment transaction details and  your complete postal address to send the courier:</strong></p>
+			<p>
+				<strong>Account Name :</strong> Sriranga Digital Software Technologies Private Limited.<br />
+				<strong>Account Number :</strong> 3950002100001647<br />
+				<strong>Account Type :</strong> Current Account<br />
+				<strong>Bank Name :</strong> Punjab National Bank<br />
+				<strong>Branch Name :</strong> SJCE Campus,Mysore<br />
+				<strong>Branch Code :</strong> 395000<br />
+				<strong>IFSC Code :</strong>  PUNB0395000<br />
+				<strong>MICR Code :</strong> 570024003
+			</p>
 			<p class="text-center mt-2"><a class="btn btn-success" href="highlights" target="_blank">Product Overview</a></p>
 			<h4 class="mt-5 px-2">Commemorating</h4>
 			<h5 class="px-2 mb-4">Gurubhaktashikhamani (गुरुभक्तशिखामणिः) Sri T K Balasubrahmanyam</h5>
